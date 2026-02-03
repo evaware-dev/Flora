5e72af1eb4bbfffec6c3edf1d6055f918f44fef0
rem53r <itznotrem@protonmail.com>
2026-02-03 19:48:00 +0200

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    `maven-publish`
 }
 
-group = "sweetie.evaware"
-version = "1.0-SNAPSHOT"
+group = "com.github.evaware-dev"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -13,11 +14,32 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    testImplementation("org.openjdk.jmh:jmh-core:1.37")
-    testAnnotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.37")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            groupId = "com.github.evaware-dev"
+            artifactId = "flora"
+            version = "1.0"
+        }
+    }
+
+    repositories {
+        maven {
+            name = "Local"
+            url = uri(layout.buildDirectory.dir("repo"))
+        }
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
